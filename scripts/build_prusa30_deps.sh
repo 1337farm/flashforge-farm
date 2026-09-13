ad59218159e7742c88c848181cc33a4a642ea62d
1337farm <324716549+1337farm@users.noreply.github.com>
2026-09-13 17:04:05 -0400

    fix(prusa30): stage Eigen unsupported/ headers (libigl needs SparseExtra)

--- scripts/build_prusa30_deps.sh
+++ scripts/build_prusa30_deps.sh
@@ -109,7 +109,7 @@ stage_include sol2 "https://github.com/ThePhD/sol2/archive/refs/tags/v3.5.0.zip"
 
 stage_include eigen "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.zip" \
     eba3f3d414d2f8cba2919c78ec6daab08fc71ba2ba4ae502b7e5d4d99fc02cda \
-    eigen.zip "Eigen"
+    eigen.zip "Eigen" "unsupported"
 
 stage_include expected "https://github.com/TartanLlama/expected/archive/refs/tags/v1.1.0.zip" \
     4b2a347cf5450e99f7624247f7d78f86f3adb5e6acd33ce307094e9507615b78 \
